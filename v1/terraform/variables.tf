3dcdee24fe6e0cfa9c0c6e61b6f2779a65846704
gTsoulos <george.d.tsoulos@gmail.com>
2022-10-17 14:14:46 +0300

    Add terraform v1 folder

---
+++ v1/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+    default = "hextrix-cluster"
+}
+
+variable "namespace_name" {
+    default = "hextrix-namespace"
+}
+
+variable "app_name" {
+    default = "hextrix-app"
+}
